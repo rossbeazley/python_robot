dba7c4582a09c09e11a064991e083ef8f47925b9
rossbeazley <ross@rossbeazley.co.uk>
2015-01-10 21:31:33 +0000

    working out run loop

---
+++ pollingRobot/Main.py
@@ -0,0 +1,12 @@
+__author__ = 'rdlb'
+
+# read whiskers
+# if left and right
+#   back off and turn right 90
+# else if left
+#   back off and turn right 45
+# else if right
+#   back off and turn left 45
+# else
+#   go forwards
+
